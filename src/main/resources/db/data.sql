1ed9fff1b0273b5cdbd6288bca4a1e50653cd195
qly <1181834839@qq.com>
2019-10-12 17:05:28 +0800

    添加用户

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,9 +1,10 @@
-INSERT INTO `user` (id, username, password, role) VALUES ('1', 'root', '1234', 'admin');
+﻿INSERT INTO `user` (id, username, password, role) VALUES ('1', 'root', '1234', 'admin');
 INSERT INTO `user` (id, username, password, role) VALUES ('2', 'user', '1234', 'user');
 INSERT INTO `user` (id, username, password, role) VALUES ('3', 'user1', '1234', 'user');
 INSERT INTO `user` (id, username, password, role) VALUES ('4', 'user2', '1234', 'user');
 INSERT INTO `user` (id, username, password, role) VALUES ('5', 'user3', '1234', 'user');
 INSERT INTO `user` (id, username, password, role) VALUES ('6', 'user4', '1234', 'user');
 INSERT INTO `user` (id, username, password, role) VALUES ('7', '测试', '1234', 'user');
+INSERT INTO `user` (id, username, password, role) VALUES ('8', 'qly', '1234', 'user');
 
 COMMIT;
